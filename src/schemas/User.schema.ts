5761e0ed4a6315ba16db88f8bf017d322ecc398f
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2021-10-31 14:18:05 +0500

    Email validation.

--- src/schemas/User.schema.ts
+++ src/schemas/User.schema.ts
@@ -5,12 +5,19 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-    @Prop()
-    name: string;
+  @Prop({ required: true })
+  name: string;
 
-    @Prop()
-    email: string;
+  @Prop({
+    required: true,
+    validate: function (email: string) {
+      return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+        email,
+      );
+    },
+  })
+  email: string;
 
-    @Prop()
-    subscribedTokens: string[]
+  @Prop({ required: true, type: [String] })
+  subscribedTokens: string[];
 }
