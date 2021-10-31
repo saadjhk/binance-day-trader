c3a98dd56bfbf38c967f520a64242baa14c0f16e
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2021-10-31 15:36:00 +0500

    Token subscription

--- src/User/User.service.ts
+++ src/User/User.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -6,13 +6,57 @@ import { User, UserDocument } from 'src/schemas/User.schema';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+    ) {}
 
-  async createUser(user: {
-    name: string;
-    email: string;
-    subscribedTokens: string[];
-  }): Promise<User> {
-    return new this.userModel(user).save();
-  }
+    async createUser(user: {
+        name: string;
+        email: string;
+        subscribedTokens: string[];
+    }): Promise<User> {
+        return new this.userModel(user).save();
+    }
+
+    async subscribeToken(email: string, token: string): Promise<string[]> {
+        const user = await this.userModel.findOne({ email });
+
+        if (!user)
+            throw new HttpException(
+                `Unable to find user having email ${email}`,
+                404,
+            );
+
+        const present = user.subscribedTokens.find((t) => t === token);
+
+        if (present)
+            throw new HttpException(
+                `Already subscribed to token ${token}`,
+                400,
+            );
+
+        user.subscribedTokens.push(token);
+
+        await user.save();
+
+        return user.subscribedTokens;
+    }
+
+    async unsubscribeToken(email: string, token: string): Promise<string[]> {
+        const user = await this.userModel.findOne({ email });
+
+        if (!user)
+            throw new HttpException(
+                `Unable to find user having email ${email}`,
+                404,
+            );
+
+        user.subscribedTokens = user.subscribedTokens.filter(
+            (t) => t !== token,
+        );
+
+        await user.save();
+
+        return user.subscribedTokens;
+    }
 }
